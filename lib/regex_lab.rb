ceae68e5e997ab6d798b37018d03029f3d9fa67d
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-07 23:03:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.split(" ").collect {|word| word.scan(/\b[A-Za-z]{5}\b/)}
+  # text.split(" ").collect {|word| word.scan(/\b[A-Za-z]{5}\b/)}
+  text.scan(/\b[A-Za-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -62,5 +63,5 @@ def valid_phone_number?(phone)
 end
 
 # puts words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
-puts words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
+# puts words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
 # puts first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
